0d4e08fffb7ef8196eb864c2da2d2e55de2d2b93
ljtang2009 <ho_ho_gl@hotmail.com>
2021-08-29 07:53:44 +0800

    添加天气主页

---
+++ pages/weather/index.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: ho_ho_gl@hotmail.com
+ * @Date: 2021-08-29 01:40:21
+ * @LastEditTime: 2021-08-29 01:41:00
+ * @LastEditors: ho_ho_gl@hotmail.com
+ * @Description: 天气首页
+ * @FilePath: \shipshape\pages\weather\index.vue
+-->
+<template>
+  <view>天气</view>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
